b62f99612f8b17015385f8804b10d676f7812f5f
Vincent Bonhomme <bonhomme.vincent@gmail.com>
2014-03-31 23:36:58 +0100

    eoa

--- workingplace.R
+++ workingplace.R
@@ -25,3 +25,20 @@ botP <- pca(botE)
 plot(botP)
 subset(bot, type=="whisky")
 
+ldk.polar <- list()
+for (i in 1:length(MGpolar)){
+  coo.i <- MGpolar$coo[[i]]
+  ldk.polar[[i]] <- coo.ldk(coo.i, 1)}
+
+res <- numeric(length(MGpolar))
+for (i in 1:length(MGpolar)) {
+  coo.i <- MGpolar$coo[[i]]
+  coo.ldk <- t(as.matrix(MGpolar$coo[[i]][ldk.polar[[i]][1], ]))
+  d <- edm(coo.i, coo.ldk)
+  ldk.polar[[i]][2] <- which.max(d)
+}
+
+coo.sample(MGpolar.al, 64)
+MGpolar.E <- eFourier(MGpolar.al, 12, norm=FALSE)
+MGpolar.P <- pca(MGpolar.E)
+plot(MGpolar.P, "sp")
