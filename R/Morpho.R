edc6d447249c75727df6493339043c56dc21c8f2
Vincent Bonhomme <bonhomme.vincent@gmail.com>
2014-04-10 18:53:06 +0200

    +bezier, bezier.i

--- R/Morpho.R
+++ R/Morpho.R
@@ -294,5 +294,38 @@ polynomials.i <- function(mod, x.pred, nb.pts=nrow(mod$model)){
 # 2.2 Cubic splines -------------------------------------------------------
 # 2.3 Bezier splines -------------------------------------------------------
 
+bezier <- function(coo, n){
+  coo <- coo.check(coo)
+  if (missing(n)) n <- nrow(coo)
+  p <- nrow(coo)
+  if (n != p) { n <- n + 1 }
+  coo1 <- coo / coo.perim.cum(coo)[p]
+  t1   <- 1 - coo.perim.cum(coo1)
+  J <- matrix(NA, p, p)
+  for (i in 1:p){
+    for (j in 1:p){
+      J[i, j] <- (factorial(p-1) / (factorial(j-1) * factorial(p-j))) *
+        (((1-t1[i])^(j-1)) * t1[i]^(p-j))}}
+  B <- ginv(t(J[, 1:n]) %*% J[,1:n])%*%(t(J[,1:n])) %*% coo
+  coo <- J[, 1:n] %*% B
+  B <- ginv(t(J[,1:n])%*%J[,1:n])%*%(t(J[,1:n])) %*% coo
+  list(J=J, B=B)}
+
+bezier.i <-function(B, nb.pts=120){
+  x   <- y <- numeric(nb.pts)
+  n    <- nrow(B)-1
+  t1   <- seq(0, 1, length=nb.pts)
+  coef <- choose(n, k=0:n)
+  b1   <- 0:n
+  b2   <- n:0
+  for (j in 1:nb.pts){
+    vectx <- vecty <- NA
+    for (i in 1:(n+1)){
+      vectx[i] <- B[i,1] * coef[i]*t1[j]^b1[i]*(1-t1[j])^b2[i]
+      vecty[i] <- B[i,2] * coef[i]*t1[j]^b1[i]*(1-t1[j])^b2[i] }
+    x[j] <- sum(vectx)
+    y[j] <- sum(vecty)}
+  coo <- cbind(x, y)
+  return(coo)}
 
 
--- workingplace.R
+++ workingplace.R
@@ -23,54 +23,6 @@ ms <- meanshapes(botE, 1)
 # bup.pi <- polynomials.i(bup.p, bup[, 1])
 # lines(bup.pi, col="red", type="b", pch=20)
 
-
-function(M,n = dim(M)[1]){
-  p<-dim(M)[1]
-  if (n != p) {n<-n+1}
-  M1<-M/coo.perim.cum(M)[p]
-  t1<-1-coo.perim.cum(M1)
-  J<-matrix(NA, p, p)
-  for (i in 1:p){
-    for (j in 1:p){J[i, j]<-(factorial(p-1)/(factorial(j-1)*factorial(p-j)))*(((1-t1[i])^(j-1))*t1[i]^(p-j))}}
-  B<-ginv(t(J[,1:n])%*%J[,1:n])%*%(t(J[,1:n]))%*%M
-  M<-J[,1:n]%*%B
-  list(J=J, B=ginv(t(J[,1:n])%*%J[,1:n])%*%(t(J[,1:n]))%*%M)}
-
-
-cumchord<-function(M)
-{cumsum(sqrt(apply((M-rbind(M[1,],M[-(dim(M)[1]),]))^2,1,sum)))}
-####f5.2
-bezier<-function(M, n = dim(M)[1])
-{
-  coo <- M
-  p<-dim(M)[1]
- if (n != p) {n<-n+1}
- M1<-coo/coo.perim.cum(coo)[p]
- t1<-1-coo.perim.cum(M1)
- J<-matrix(NA, p, p)
- for (i in 1:p){
-   for (j in 1:p){J[i, j]<-(factorial(p-1)/(factorial(j-1)*factorial(p-j)))*(((1-t1[i])^(j-1))*t1[i]^(p-j))}}
- B<-ginv(t(J[,1:n])%*%J[,1:n])%*%(t(J[,1:n]))%*%M
- M<-J[,1:n]%*%B
- list(J=J, B=ginv(t(J[,1:n])%*%J[,1:n])%*%(t(J[,1:n]))%*%M)}
-####f5.3
-beziercurve<-function(B,p)
-{X<-Y<-numeric(p)
- n<-dim(B)[1]-1
- t1<-seq(0,1,length=p)
- coef<-choose(n, k=0:n)
- b1<-0:n; b2<-n:0
- for (j in 1:p)
- {vectx<-vecty<-NA
-  for (i in 1:(n+1))
-  {vectx[i]<-B[i,1]*coef[i]*t1[j]^b1[i]*(1-t1[j])^b2[i]
-   vecty[i]<-B[i,2]*coef[i]*t1[j]^b1[i]*(1-t1[j])^b2[i]}
-  X[j]<-sum(vectx)
-  Y[j]<-sum(vecty)}
- cbind(X, Y)}
-
 b <- coo.scale(coo.center(coo.sample(bot[5], 12)))
-coo.plot(b)
-b.b <- bezier(b)
-b.i <- beziercurve(b.b$B, 120)
-lines(b.i, col="red")
+
+
