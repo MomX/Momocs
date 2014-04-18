79b0140d2154e8452b113a99292c85354b263340
Vincent Bonhomme <bonhomme.vincent@gmail.com>
2014-04-18 11:03:35 +0200

    help files + structure

--- R/domestic.R
+++ R/domestic.R
@@ -83,59 +83,6 @@ NULL
 # #' from allowing us to share the data.
 # NULL
 
-# 1. Morphospace functions -----------------------------------------------------
-pca2shp.efourier <- function (pos, rot, mshape, amp=1, nb.pts=60, trans=TRUE) {
-  if (ncol(pos) != ncol(rot)) stop("'rot' and 'pos' must have the same ncol")
-  if(length(mshape) != nrow(rot)) stop("'mshape' and ncol(rot) lengths differ")
-  # stupid function
-  mprod <- function(m, s){
-    res <- m
-    for (i in 1:ncol(m)) { res[, i] <- m[, i]*s[i] }
-    return(res)}
-  nb.h <- length(mshape)/4
-  n  <- nrow(pos)
-  # we prepare the array
-  res <- list()
-  for (i in 1:n) {
-    ax.contrib <- mprod(rot, pos[i, ])*amp
-    coe        <- mshape + apply(ax.contrib, 1, sum)
-    xf         <- coeff.split(coe)
-    coo        <- efourier.i(xf, nb.h = nb.h, nb.pts=nb.pts)
-    if (trans) {coo <- coo.trans(coo, x=pos[i, 1], y=pos[i, 2])}
-  res[[i]] <- coo}
-  return(res)}
-
-pos.shapes <- function(xy, pos.shp=c("range", "circle")[1],
-                       nb.shp=12, nr.shp=6, nc.shp=5, circle.r.shp){
-  if (is.data.frame(pos.shp) | is.matrix(pos.shp)) {
-    return(as.matrix(pos.shp))}
-  if (pos.shp=="circle") {
-    if (missing(circle.r.shp)) {
-      # mean distance from origin
-      circle.r.shp <- mean(apply(xy, 1, function(x) sqrt(sum(x^2))))}
-    t <- seq(0, 2*pi, len=nb.shp+1)[-(nb.shp+1)]
-    pos <- cbind(circle.r.shp*cos(t), circle.r.shp*sin(t))
-    colnames(pos) <- c("x", "y") # pure cosmetics
-    return(pos)}
-  if (pos.shp=="range") {
-    pos <- expand.grid(seq(min(xy[, 1]), max(xy[, 1]), len=nr.shp),
-                       seq(min(xy[, 2]), max(xy[, 2]), len=nc.shp))
-    pos <- as.matrix(pos)
-    colnames(pos) <- c("x", "y") # pure cosmetics
-    return(pos)}
-  if (pos.shp=="full") {
-#     w <- par("usr")
-#     pos <- expand.grid(seq(w[1], w[2], len=nr.shp),
-#                        seq(w[3], w[4], len=nc.shp))
-         w <- par("usr")
-         pos <- expand.grid(seq(par("xaxp")[1]*0.9, par("xaxp")[2]*0.9, len=nr.shp),
-                            seq(par("yaxp")[1]*0.9, par("yaxp")[2]*0.9, len=nc.shp))
-    pos <- as.matrix(pos)
-    colnames(pos) <- c("x", "y") # pure cosmetics
-    return(pos)   
-  }
-  # if a non-valid method is passed
-  return(xy)}
 
 # 2. Domestic functions -------------------------------------------------------------
 #' Calculates euclidean distance between two points.
--- R/graphics.R
+++ R/graphics.R
@@ -640,7 +640,83 @@ tps.iso <- function(fr, to, amp=1, palette = col.summer,
     coo.draw(to, border=shp.border[2], col=shp.col[2],
              lwd=shp.lwd[2], lty=shp.lty[2])}}
 
-# 5. Color palettes ------------------------------------------------------------
+# 5. Morphospace functions -----------------------------------------------------
+#' Returns shape for a point on a PC plan
+#' 
+#' todo
+#' @export pca2shp.efourier
+#' @param pos the position on the plan
+#' @param rot the loadings
+#' @param mshape the mean shape
+#' @param amp the amplification factor
+#' @param nb.pts the number of point for drawing the shapes
+#' @param trans logical
+#' @keywords graphics
+pca2shp.efourier <- function (pos, rot, mshape, amp=1, nb.pts=60, trans=TRUE) {
+  if (ncol(pos) != ncol(rot)) stop("'rot' and 'pos' must have the same ncol")
+  if(length(mshape) != nrow(rot)) stop("'mshape' and ncol(rot) lengths differ")
+  # stupid function
+  mprod <- function(m, s){
+    res <- m
+    for (i in 1:ncol(m)) { res[, i] <- m[, i]*s[i] }
+    return(res)}
+  nb.h <- length(mshape)/4
+  n  <- nrow(pos)
+  # we prepare the array
+  res <- list()
+  for (i in 1:n) {
+    ax.contrib <- mprod(rot, pos[i, ])*amp
+    coe        <- mshape + apply(ax.contrib, 1, sum)
+    xf         <- coeff.split(coe)
+    coo        <- efourier.i(xf, nb.h = nb.h, nb.pts=nb.pts)
+    if (trans) {coo <- coo.trans(coo, x=pos[i, 1], y=pos[i, 2])}
+    res[[i]] <- coo}
+  return(res)}
+
+#' Calculates nice positions on a plan for drawing shapes
+#' 
+#' todo
+#' @export pos.shapes
+#' @param xy todo
+#' @param pos.shp the way shape should be positionned
+#' @param nb.shp the total number of shapes
+#' @param nr.shp the number of rows to position shapes
+#' @param nc.shp the number of cols to position shapes
+#' @param circle.r.shp if circle, its radius
+#' @keywords graphics
+pos.shapes <- function(xy, pos.shp=c("range", "circle")[1],
+                       nb.shp=12, nr.shp=6, nc.shp=5, circle.r.shp){
+  if (is.data.frame(pos.shp) | is.matrix(pos.shp)) {
+    return(as.matrix(pos.shp))}
+  if (pos.shp=="circle") {
+    if (missing(circle.r.shp)) {
+      # mean distance from origin
+      circle.r.shp <- mean(apply(xy, 1, function(x) sqrt(sum(x^2))))}
+    t <- seq(0, 2*pi, len=nb.shp+1)[-(nb.shp+1)]
+    pos <- cbind(circle.r.shp*cos(t), circle.r.shp*sin(t))
+    colnames(pos) <- c("x", "y") # pure cosmetics
+    return(pos)}
+  if (pos.shp=="range") {
+    pos <- expand.grid(seq(min(xy[, 1]), max(xy[, 1]), len=nr.shp),
+                       seq(min(xy[, 2]), max(xy[, 2]), len=nc.shp))
+    pos <- as.matrix(pos)
+    colnames(pos) <- c("x", "y") # pure cosmetics
+    return(pos)}
+  if (pos.shp=="full") {
+    #     w <- par("usr")
+    #     pos <- expand.grid(seq(w[1], w[2], len=nr.shp),
+    #                        seq(w[3], w[4], len=nc.shp))
+    w <- par("usr")
+    pos <- expand.grid(seq(par("xaxp")[1]*0.9, par("xaxp")[2]*0.9, len=nr.shp),
+                       seq(par("yaxp")[1]*0.9, par("yaxp")[2]*0.9, len=nc.shp))
+    pos <- as.matrix(pos)
+    colnames(pos) <- c("x", "y") # pure cosmetics
+    return(pos)   
+  }
+  # if a non-valid method is passed
+  return(xy)}
+
+# 6. Color palettes ------------------------------------------------------------
 
 #' Some color palettes.
 #' @name col.summer
