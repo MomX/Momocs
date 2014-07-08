9813a6c22d0ec94bcc8511492b94ccdfd56e5c65
Vincent Bonhomme <bonhomme.vincent@gmail.com>
2014-07-09 00:13:03 +0100

    cosmetics

--- R/graphics-misc.R
+++ R/graphics-misc.R
@@ -4,21 +4,28 @@
 #' @name col.summer
 #' @title Some color palettes.
 #' @rdname color_palettes
+#' @aliases Palettes
 #' @param n the number of colors to generate from the color palette
 #' @return colors (hexadecimal format)
+#' @note \code{col.solarized} is based on Solarized: \url{http://ethanschoonover.com/solarized}; 
+#' \code{col.div} on ColorBrewer2: \url{http://colorbrewer2.org/}; \code{col.gallus2} is based on
+#' Sron \url{http://www.sron.nl/~pault/colourschemes.pdf}
 #' @keywords Graphics
 #' @examples
-#' barplot(1:10, col=col.summer(10), main="col.summer")
-#' barplot(1:10, col=col.spring(10), main="col.spring")
-#' barplot(1:10, col=col.autumn(10), main="col.autumn")
-#' barplot(1:10, col=col.solarized(10), main="col.solarized")
-#' barplot(1:10, col=col.gallus(10), main="col.gallus")
-#' barplot(1:10, col=col.blackgallus(10), main="col.blackgallus")
-#' barplot(1:10, col=col.hot(10), main="col.hot")
-#' barplot(1:10, col=col.cold(10), main="col.cold")
-#' barplot(1:10, col=col.sari(10), main="col.sari")
-#' barplot(1:10, col=col.india(10), main="col.india")
-#' barplot(1:10, col=col.bw(10), main="col.bw")
+#' barplot(1:20, col=col.summer(20), main="col.summer")
+#' barplot(1:20, col=col.summer2(20), main="col.summer2")
+#' barplot(1:20, col=col.spring(20), main="col.spring")
+#' barplot(1:20, col=col.autumn(20), main="col.autumn")
+#' barplot(1:20, col=col.solarized(20), main="col.solarized")
+#' barplot(1:20, col=col.gallus(20), main="col.gallus")
+#' barplot(1:20, col=col.gallus2(20), main="col.gallus2")
+#' barplot(1:20, col=col.blackgallus(20), main="col.blackgallus")
+#' barplot(1:20, col=col.hot(20), main="col.hot")
+#' barplot(1:20, col=col.cold(20), main="col.cold")
+#' barplot(1:20, col=col.sari(20), main="col.sari")
+#' barplot(1:20, col=col.india(20), main="col.india")
+#' barplot(1:20, col=col.bw(20), main="col.bw")
+#' barplot(1:20, col=col.div(20), main="col.div")
 #' @export
 col.summer <- colorRampPalette(c("#4876FF", "#FFFF00", "#FF3030"))
 #' @rdname color_palettes
@@ -33,16 +40,16 @@ col.solarized <- colorRampPalette(c("#b58900", "#cb4b16", "#dc322f", "#d33682",
                                     "#6c71c4", "#268bd2", "#2aa198", "#859900"))
 #' @rdname color_palettes
 #' @export
-col.gallus <- colorRampPalette(c("#025D8C", "#FFFFFF", "#A80000"))
+col.gallus <- colorRampPalette(c("#0000FF", "#FFFFFF", "#CC0000"))
 #' @rdname color_palettes
 #' @export
-col.blackgallus <- colorRampPalette(c("#000080", "#000000", "#EE0000"))
+col.blackgallus <- colorRampPalette(c("#0000FF", "#272727", "#CC0000"))
 #' @rdname color_palettes
 #' @export
-col.hot <- colorRampPalette(c("#F2F2F2","#A80000"))
+col.hot <- colorRampPalette(c("#F2F2F2","#CC0000"))
 #' @rdname color_palettes
 #' @export
-col.cold <- colorRampPalette(c("#F2F2F2","#025D8C"))
+col.cold <- colorRampPalette(c("#F2F2F2","#0000FF"))
 #' @rdname color_palettes
 #' @export
 col.sari <- colorRampPalette(c("#551A8B", "#FF7F00"))
@@ -52,5 +59,20 @@ col.india <- colorRampPalette(c("#FF9933", "#138808"))
 #' @rdname color_palettes
 #' @export
 col.bw <- colorRampPalette(c("#FFFFFF", "#000000"))
+#' @rdname color_palettes
+#' @export
+col.div <- colorRampPalette(c("#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C",
+                              "#FB9A99", "#E31A1C", "#FDBF6F", "#FF7F00",
+                              "#CAB2D6", "#6A3D9A", "#FFFF99", "#B15928"))
+#' @rdname color_palettes
+#' @export
+col.gallus2 <- colorRampPalette(c("#3D52A1", "#3A89C9", "#77B7E5", "#B4DDF7",
+                                  "#E6F5FE", "#FFFAD2", "#FFE3AA", "#F9BD7E",
+                                  "#ED875E", "#D24D3E", "#AE1C3E")) 
+#' @rdname color_palettes
+#' @export
+col.summer2 <- colorRampPalette(c("#781C81", "#413B93", "#4065B1", "#488BC2",
+                                  "#55A1B1", "#63AD99", "#7FB972", "#B5BD4C",
+                                  "#D9AD3C", "#E68E34", "#E6642C", "#D92120"))
 
 ##### end colors graphics
