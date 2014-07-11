b41ff4aaba0d38164a591a21d73c8c8382fd6853
Vincent Bonhomme <bonhomme.vincent@gmail.com>
2014-07-11 17:28:25 +0100

    various cosmetics

--- R/graphics-layers.R
+++ R/graphics-layers.R
@@ -28,13 +28,21 @@
 #rug
 #' @export
 .rug <- function(xy, fac, col){
+  xy <- jitter(xy, 5)
+  g <- 0.0075
+  y0 <- par("usr")[3]
+  x0 <- par("usr")[1]
+  gy <- (par("usr")[4] - par("usr")[3])*g
+  gx <- (par("usr")[2] - par("usr")[1])*g #*(.wdw()[2] / .wdw()[1])
+  #   gy <- 0
+  #   gx <- 0
   if (is.null(fac)) {
-    rug(xy[, 1], ticksize=0.005, side=1, col="black", quiet=TRUE)
-    rug(xy[, 2], ticksize=0.005, side=2, col="black", quiet=TRUE)
+    rug(xy[, 1], ticksize=g, side=1, col="black", lend=1, quiet=TRUE)
+    rug(xy[, 2], ticksize=g, side=2, col="black", lend=1, quiet=TRUE)
   } else {
     for (i in seq(along=levels(fac))) {
-      rug(xy[fac==levels(fac)[i], 1], ticksize=0.005, lwd=1, side=1, col=col[i], quiet=TRUE)
-      rug(xy[fac==levels(fac)[i], 2], ticksize=0.005, lwd=1,  side=2, col=col[i], quiet=TRUE)}}}
+      rug(xy[fac==levels(fac)[i], 1], ticksize=g, pos=y0+(i-1)*gy , side=1, col=.transp(col[i], 0), lwd=1.2, lend=1, quiet=TRUE)
+      rug(xy[fac==levels(fac)[i], 2], ticksize=g, pos=x0+(i-1)*gx,  side=2, col=.transp(col[i], 0), lwd=1.2, lend=1, quiet=TRUE)}}}
 
 #confidence ellipses
 #' @export
@@ -49,16 +57,24 @@
 
 #confidence ellipses
 #' @export
-.ellipsesax <- function(xy, fac, conf, col, lty, lwd=1){
+.ellipsesax <- function(xy, fac, conf, col, lty=1, lwd=1){
+  if (length(conf)>1){
+    conf <- sort(conf, decreasing = FALSE)
+    if (missing(lwd) | length(lwd)!= length(conf)){
+      lwd <-  rev(seq(0, max(lwd), length=(length(conf)+1))[-1])
+      lwd <-  lwd^1.5 # just to amplify things
+    }
+  }
   for (i in seq(along=levels(fac))) {
     pts.i <- xy[fac==levels(fac)[i], ]
     if (is.matrix(pts.i)) {
       if (nrow(pts.i)>1) {
-        seg.i <- conf.ell(x=pts.i, conf=conf, nb.pts=720)$seg
-        segments(seg.i[1, 1], seg.i[1, 2],
-                 seg.i[2, 1], seg.i[2, 2], lty=lty, col=col[i], lwd=lwd)
-        segments(seg.i[3, 1], seg.i[3, 2],
-                 seg.i[4, 1], seg.i[4, 2], lty=lty, col=col[i], lwd=lwd)}}}}
+        for (j in seq(along=conf)){
+          seg.i <- conf.ell(x=pts.i, conf=conf[j], nb.pts=720)$seg
+          segments(seg.i[1, 1], seg.i[1, 2],
+                   seg.i[2, 1], seg.i[2, 2], lty=lty, col=col[i], lwd=lwd[j])
+          segments(seg.i[3, 1], seg.i[3, 2],
+                   seg.i[4, 1], seg.i[4, 2], lty=lty, col=col[i], lwd=lwd[j])}}}}}
 
 #convex hulls
 #' @export
@@ -79,23 +95,23 @@
     if (is.matrix(cent.i)){
       cent.i <- coo.centpos(xy[fac==levels(fac)[i], ])}
     cent[i, ] <- cent.i}
-#   if (thigmophobe & nlevels(fac)>2){
-#     thigmophobe.labels(cent[, 1], cent[, 2], labels = levels(fac), col=col)
-#   } else {
-    labels <- levels(fac)
+  #   if (thigmophobe & nlevels(fac)>2){
+  #     thigmophobe.labels(cent[, 1], cent[, 2], labels = levels(fac), col=col)
+  #   } else {
+  labels <- levels(fac)
+  if (abbreviate) labels <- abbreviate(labels, minlength = 1)
+  p <- strheight(labels[1])
+  if (rect){
     if (abbreviate) labels <- abbreviate(labels, minlength = 1)
-    p <- strheight(labels[1])
-    if (rect){
-      if (abbreviate) labels <- abbreviate(labels, minlength = 1)
-      w <- strwidth(labels, cex=cex)
-      h <- strheight(labels, cex=cex)
-      rect(xleft  = cent[, 1]-w/2 -p, ybottom = cent[, 2]-h/2 +p,
-           xright = cent[, 1]+w/2 +p, ytop    = cent[, 2]+h/2 +p,
-           col = .transp("#FFFFFF", 0.5), border = NA)
-      text(cent[, 1], cent[, 2] +p, labels=labels, cex=cex, col=col)
-    } else {
-    text(cent[, 1], cent[, 2] +p, labels=labels, col=col, cex=cex)}
-# }
+    w <- strwidth(labels, cex=cex)
+    h <- strheight(labels, cex=cex)
+    rect(xleft  = cent[, 1]-w/2 -p, ybottom = cent[, 2]-h/2 +p,
+         xright = cent[, 1]+w/2 +p, ytop    = cent[, 2]+h/2 +p,
+         col = .transp("#FFFFFF", 0.5), border = NA)
+    text(cent[, 1], cent[, 2] +p, labels=labels, cex=cex, col=col, font=2)
+  } else {
+    text(cent[, 1], cent[, 2] +p, labels=labels, col=col, cex=cex, font=2)}
+  # }
 }
 
 #add 'stars'
@@ -110,7 +126,7 @@
 
 #add density
 #' @export
-.density <- function(xy, fac, density.lev, col, n.kde2d = 50){
+.density <- function(xy, fac, levels, col, transp, n.kde2d = 50){
   if (missing(fac) | is.null(fac)) fac <- factor(rep("f", nrow(xy)))
   #   z0 <- kde2d(xy[, 1], xy[, 2], n=n.kde2d, lims = c(par("usr")))$z
   #   zmin <- min(z0)
@@ -124,11 +140,11 @@
       ki$z <- .normalize(ki$z)
       image(ki$x, ki$y, ki$z, add=TRUE,
             xlim=range(ki$x), ylim=range(ki$y), 
-            col=col.transp(density.lev, col[i], 0.25))}}}
+            col=col.transp(levels, col[i], transp))}}}
 
 #add contour lines
 #' @export
-.contour <- function(xy, fac, contour.lev, col, n.kde2d = 50){
+.contour <- function(xy, fac, levels, col, transp, n.kde2d = 50){
   if (missing(fac) | is.null(fac)) fac <- factor(rep("f", nrow(xy)))
   #   z0 <- kde2d(xy[, 1], xy[, 2], n=n.kde2d, lims = c(par("usr")))$z
   #   zmin <- min(z0)
@@ -141,8 +157,8 @@
       #       ki$z <- .normalize(ki$z, zmin, zmax)
       ki$z <- .normalize(ki$z)
       contour(ki$x, ki$y, ki$z, add=TRUE,
-              nlevels=contour.lev, drawlabels=FALSE, 
-              col=.transp(rep(col[i], contour.lev), 0.5))}}}
+              nlevels=levels, drawlabels=FALSE, 
+              col=.transp(rep(col[i], levels), transp))}}}
 
 #add delaunay triangulation
 #' @export
@@ -152,8 +168,8 @@
     xy.i <- xy[fac==levels(fac)[i], ]
     if (is.matrix(xy.i)){
       if (nrow(xy.i)>3) {
-      links.i <- links.delaunay(xy.i)
-      ldk.links(xy.i, links.i, col=.transp(col[i], 2/3), lwd=1.5)}}}}
+        links.i <- links.delaunay(xy.i)
+        ldk.links(xy.i, links.i, col=.transp(col[i], 2/3), lwd=1.5)}}}}
 
 #add loading vectors
 #' @export
@@ -168,25 +184,25 @@
 
 #add eigen
 #' @export
-.eigen <- function(ev, xax, yax, ratio=0.12, ev.names){
+.eigen <- function(ev, xax, yax, ratio=0.08, ev.names){
   plt0 <- par("plt")
   on.exit(par(plt = plt0))
   g <- 0.015
   w <- min(c(plt0[2]-plt0[1]), plt0[4]-plt0[3])*ratio
   par(plt = c(plt0[2]-w-g, plt0[2]-g, plt0[3]+g*1.5, plt0[3]+w+g*1.5), xpd=NA, new = TRUE)
   cols <- rep("grey80", 5)
-  cols[c(xax,yax)] <- "grey40"
+  cols[c(xax, yax)] <- "grey40"
   var <- ev^2
   cs.var <- cumsum(var)/sum(var)
   k <- ifelse(max(c(xax, yax))>5, max(c(xax, yax)), 5)
   barplot(var[1:k], axes=FALSE, col=cols, border=NA)
-  text(-0.9, par("usr")[3], labels=ev.names,
-       pos=4, cex=0.6, srt=90, col="grey40")}
+  text(-1, par("usr")[3], labels=ev.names,
+       pos=4, cex=2/3, srt=90, col="grey40")}
 
 #names axes
 #' @export
 .axisnames <- function(xax, yax, nax="PC"){
-  cex <- 0.7
+  cex <- 2/3
   #gx <- strwidth("PCN", cex=cex)/1.5
   gy <- strheight("PCN", cex=cex)/1.5
   gx <- strwidth("00.0%", cex=cex)/1.5 # center / relatively to var %
