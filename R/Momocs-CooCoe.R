68dbeddb5426395de164c68cf20ec8fa2f5b0ed2
Vincent Bonhomme <bonhomme.vincent@gmail.com>
2014-08-07 12:39:49 +0100

    .

--- R/Momocs-CooCoe.R
+++ R/Momocs-CooCoe.R
@@ -1,36 +1,42 @@
 ##### Coo and Coe classes declarations, domestic functions and
 ##### helpers.
 
-# 0. Coo (Opn/Out/Ldk) domestics
-# -----------------------------------------------
+# 1. Coo -----------------------------------------------
 
 #' Coo class
-#' 
+#'
 #' \code{Coo} class is a 'super class' for \link{Out}, \code{\link{Opn}} and \link{Ldk} classes,
 #' for handling \bold{closed outlines}, \bold{open outlines} and \bold{configuration of landmarks}, respectively.
-#' 
+#'
 #' This class allows to recycle most of the methods, since all of them apply on
 #' (x; y) coordinates stored \code{$coo} slot. All of them also have a \code{$fac} component,
 #' yet not mandatory, to store grouping informations (also called classifiers). Others slots
 #' may be present in certain classes.
-#' 
+#'
 #' In other words, \code{Out}, \code{Out} and \code{Ldk} classes
-#' are all, primarily, \code{Coo} objects on which we define generic \emph{and} 
+#' are all, primarily, \code{Coo} objects on which we define generic \emph{and}
 #' specific methods.
-#' 
+#'
 #' More, generally, see \link{Out}, \link{Opn} and \link{Ldk} for documentation on these classes.
 #' And Momocs' vignette for more detailed informations on the Momocs' 'architecture'.
-#' 
-#' Finally, note that if you used Momocs before version <0.9, and/or if you have read the JSS paper,
-#' you are probably looking for \link{Out}. If you have 'old' Coo files, eg saved as \code{.rda} files,
-#' you can import them by: i) opening a new R session; ii) load your file, say 'foo', iii) load Momocs,
-#' iv) type: \code{Out(foo@@coo, fac=foo@@fac)}, same thing for the \code{@@ldk} slot, if any.
-#' 
+#'
 #' You can access all the methods available for Coo objects with \code{methods(class=Coo)}.
 #' Among them, some are not individually documented but you will find an exhaustive list
 #' below, as well as in individual  Momocs' vignettes.
-#' 
+#'
 #' @param ... anything and, anyway, this function will simply returns a message.
+#' @note
+#' If you used Momocs before version <0.9, and/or if you have read the JSS paper,
+#' you are probably looking for \link{Out}. If you have "old" Coo files, e.g. saved as \code{.rda} files,
+#' no worry, you can import them:
+#' \enumerate{
+#' \item open a new R session or detach Momocs: \code{detach(package:Momocs, unload=TRUE)}
+#' \item load your file: \code{load("foo.rda")}
+#' It may produce an error but the \code{.rda} has been loaded (see \code{ls()})
+#' \item load the last version of Momocs
+#' \item type: \code{foo2 <- Out(foo@@coo, fac=foo@@fac)}, same thing for the \code{@@ldk} slot and others, if any.
+#' \item et voila!
+#'}
 #' @examples
 #' # to see all methods for Coo objects.
 #' methods(class='Coo')
@@ -45,12 +51,12 @@
 #' plot(bot)
 #' }
 #' # Getters (you can also use it to set data)
-#' bot[1] # equivalent to bot[[1]]
+#' bot[1]
 #' # access the different components
 #' # $coo coordinates
-#' bot$coo
+#' head(bot$coo)
 #' # $fac grouping factors
-#' bot$fac
+#' head(bot$fac)
 #' # table
 #' table(bot$fac)
 #' # an internal view of an Out object
@@ -71,20 +77,20 @@ Coo <- function(...) {
 }
 
 #' Coe class
-#' 
+#'
 #' \code{Coe} class is a 'super class' for \link{OutCoe} \link{OpnCoe} and \link{LdkCoe} classes,
 #' matrices of coefficients (in their \code{$coe} slot), along with other informations,
-#' (e.g. an inherited \code{$fac}) obtained with morphometrics methods on 
+#' (e.g. an inherited \code{$fac}) obtained with morphometrics methods on
 #' \link{Out}, \link{Opn} and \link{Ldk} objects.
-#' 
+#'
 #' This classes allows to recycle most of the methods, since both of them apply on
 #' matrices of coefficients. In other words, \code{OutCoe}, \code{OpnCoe} and \code{LdkCoe}
-#' classes are all, primarily, \code{Coe} objects on which we define generic 
+#' classes are all, primarily, \code{Coe} objects on which we define generic
 #' \emph{and} specific methods.
-#' 
+#'
 #' All these classes contain a \code{$coe} slot where the coefficients are stored,
 #' and can be accessed.
-#' 
+#'
 #' More, generally, see \link{OutCoe}, \link{OpnCoe} and \link{LdkCoe}
 #' for documentation on these classes.
 #'
@@ -92,28 +98,28 @@ Coo <- function(...) {
 #' @examples
 #' # to see all methods for Coo objects.
 #' methods(class='Coe')
-#' 
+#'
 #' data(bot)
 #' bot.f<- eFourier(bot, 12)
 #' bot.f
 #' class(bot.f)
-#' 
+#'
 #' # if you want to work directly on the matrix of coefficients
 #' bot.f$coe
-#' 
+#'
 #' data(olea)
 #' op <- orthoPolynomials(olea, 5)
 #' op
 #' class(op)
 #' op$coe # same thing
-#' 
+#'
 #' data(wings)
 #' wp <- fgProcrustes(wings, tol=1e-4)
 #' wp
 #' class(wp) # for Ldk methods, LdkCoe objects can also be considered as Coo objects
 #' # so you can apply all Ldk methods available.
 #' wp$coe # Procrustes aligned coordinates
-#' 
+#'
 #' @export
 Coe <- function(...) {
     cat(" * Coe constructor does not exist alone. See ?Coe")
@@ -148,26 +154,12 @@ str.Coe <- function(object, ...) {
     }
 }
 
-#' @export
-"[[.Coo" <- function(x, i, ...) {
-    if (missing(i)) {
-        return(x$coo[])
-    }
-    if (is.integer(i)) {
-        return(x$coo[i])
-    }
-    if (is.numeric(i)) {
-        return(x$coo[[i]])
-    }
-}
 
-# length on an Coo return the length of Coo$coo, ie the
-# number of coordinates
 #' @export
 length.Coo <- function(x) {
     Coo <- x
-    return(length(Coo$coo))
-}
+    return(length(Coo$coo))}
+
 #' @export
 dim.Coo <- function(x) {
     return(length(Coo$coo))
@@ -200,4 +192,4 @@ names.Coe <- function(x) {
 "names<-.Coe" <- function(x, value) {
     rownames(x$coe) <- value
     return(x)
-} 
+}
