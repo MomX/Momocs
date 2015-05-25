814f469490e3375f78f481bb3d3fa23491f4ddc4
vbonhomme <bonhomme.vincent@gmail.com>
2015-05-25 13:01:53 +0200

    Southampton ready

--- R/mult-KMEANS.R
+++ R/mult-KMEANS.R
@@ -1,21 +1,28 @@
-#' k-means
+#' KMEANS on PCA objects
 #'
-#' A very basic and prototypic implementation of k-means
+#' A very basic and prototypic implementation of k-means.
+#' Beware that morphospaces are calculated so far for the 1st and 2nd
+#' component.
 #' @param x PCA object
 #' @param centers numeric number of centers
 #' @param nax numeric the range of PC components to use (1:2 by default)
+#' @param pch to draw the points
+#' @param cex to draw the points
 #' @param ... additional arguments to be passed to \link{kmeans}
 #' @return the same thing as \link{kmeans}
 #' @examples
 #' data(bot)
 #' bp <- PCA(efourier(bot, 10))
 #' KMEANS(bp, 2)
+#' @rdname KMEANS
 #' @export
 KMEANS <- function(x, ...){
   UseMethod("KMEANS")
 }
+
+#' @rdname KMEANS
 #' @export
-KMEANS.PCA <- function(x, centers, nax=1:2, ...){
+KMEANS.PCA <- function(x, centers, nax=1:2, pch=20, cex=0.5, ...){
   xy <- x$x[, nax]
   km <- kmeans(xy, centers, ...)
   clusts <- factor(km$cluster)
@@ -26,9 +33,10 @@ KMEANS.PCA <- function(x, centers, nax=1:2, ...){
   .frame(xy, center.origin = TRUE, zoom = 1)
   .grid(5)
   .chull(xy, as.factor(km$cluster), col = cols, lty=2)
-  points(xy, pch=20, cex=0.5, col=cols[clusts])
+  points(xy, pch=pch, cex=cex, col=cols[clusts])
   points(km$centers, pch=3, col=cols)
-  Momocs:::morphospacePCA(bp, 1, 2, pos.shp=km$centers)
+  morphospacePCA(x, 1, 2, pos.shp=km$centers,
+                          col.shp = "#00000011", border.shp="#000000")
   box()
   return(km)
 }
