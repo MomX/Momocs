a20c540f88f17f8960ea52c56c2c01bfe55c92bb
vbonhomme <bonhomme.vincent@gmail.com>
2015-06-08 14:21:17 +0200

    plop

---
+++ update.R
@@ -0,0 +1,15 @@
+#update
+currDir <- getwd() # this should be the top level directory of the git repo
+currDCF <- read.dcf("DESCRIPTION")
+currVersion <- currDCF[1,"Version"]
+splitVersion <- strsplit(currVersion, ".", fixed=TRUE)[[1]]
+nVer <- length(splitVersion)
+currEndVersion <- as.integer(splitVersion[nVer])
+newEndVersion <- as.character(currEndVersion + 1)
+splitVersion[nVer] <- newEndVersion
+newVersion <- paste(splitVersion, collapse=".")
+currDCF[1,"Version"] <- newVersion
+currDCF[1, "Date"] <- strftime(as.POSIXlt(Sys.Date()), "%Y-%m-%d")
+write.dcf(currDCF, "DESCRIPTION")
+system("git add DESCRIPTION")
+cat("Incremented package version and added to commit!\n")
