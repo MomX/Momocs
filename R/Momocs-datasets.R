eea821db4159efb867e07d7cde60d3ed1684abf5
vbonhomme <bonhomme.vincent@gmail.com>
2015-08-03 12:13:48 +0200

    #93

--- R/Momocs-datasets.R
+++ R/Momocs-datasets.R
@@ -1,7 +1,7 @@
 ##### Dataset documentation and declaration
 
 #' Data: Outline coordinates of beer and whisky bottles.
-#' 
+#'
 #' @docType data
 #' @name bot
 #' @rdname data_bot
@@ -14,7 +14,7 @@
 NULL
 
 #' Data: Outline coordinates of cephalic outlines of trilobite
-#' 
+#'
 #' @docType data
 #' @name trilo
 #' @rdname data_trilo
@@ -22,12 +22,12 @@ NULL
 #' @format A \link{Out} object 64 coordinates of 50 cephalic outlines from different
 #' ontogenetic stages of trilobite.
 #' @source  Arranged from: \url{http://folk.uio.no/ohammer/past/outlines.dat}.
-#' The original data included 51 outlines and 5 ontogenetic stages, 
+#' The original data included 51 outlines and 5 ontogenetic stages,
 #' but one of them has just a single outline thas has been removed.
 NULL
 
 #' Data: Outline coordinates of mosquito wings.
-#' 
+#'
 #' @docType data
 #' @name mosquito
 #' @rdname data_mosquito
@@ -40,7 +40,7 @@ NULL
 NULL
 
 #' Data: Outline coordinates of hand-drawn hearts
-#' 
+#'
 #' @docType data
 #' @name hearts
 #' @rdname data_hearts
@@ -52,7 +52,7 @@ NULL
 NULL
 
 #' Data: Outline coordinates of olive seeds open outlines.
-#' 
+#'
 #' @docType data
 #' @name olea
 #' @rdname data_olea
@@ -60,17 +60,17 @@ NULL
 #' @format An \link{Opn} object with the outline coordinates of olive seeds.
 #' @source We thank Jean-Frederic Terral and Sarah Ivorra (UMR CBAE, Montpellier, France)
 #' from allowing us to share the data.
-#' 
+#'
 #' You can have a look to the original paper:
-#' Terral J-F, Alonso N, Capdevila RB i, Chatti N, Fabre L, Fiorentino G, 
-#' Marinval P, Jorda GP, Pradat B, Rovira N, et al. 2004. 
-#' Historical biogeography of olive domestication (\emph{Olea europaea} L.) 
-#' as revealed by geometrical morphometry applied to biological and 
+#' Terral J-F, Alonso N, Capdevila RB i, Chatti N, Fabre L, Fiorentino G,
+#' Marinval P, Jorda GP, Pradat B, Rovira N, et al. 2004.
+#' Historical biogeography of olive domestication (\emph{Olea europaea} L.)
+#' as revealed by geometrical morphometry applied to biological and
 #' archaeological material. \emph{Journal of Biogeography} \bold{31}: 63-77.
 NULL
 
 #' Data: Outline coordinates of various shapes
-#' 
+#'
 #' @docType data
 #' @name shapes
 #' @rdname data_shapes
@@ -80,44 +80,57 @@ NULL
 NULL
 
 #' Data: Landmarks coordinates of mosquito wings
-#' 
+#'
 #' @docType data
 #' @name wings
 #' @rdname data_wings
 #' @keywords Datasets
-#' @format A \link{Ldk} object containing 18 (x; y) landmarks  from 127 mosquito wings, from 
+#' @format A \link{Ldk} object containing 18 (x; y) landmarks  from 127 mosquito wings, from
 #' @source Rohlf and Slice 1990 and \url{http://life.bio.sunysb.edu/morph/data/RohlfSlice1990Mosq.nts}
 NULL
 
 #' Data: Configuration of landmarks of oak leaves
-#' 
+#'
 #' From Viscosi and Cardini (2001).
 #' @docType data
 #' @name oak
 #' @rdname data_oak
 #' @keywords Datasets
-#' @format A \link{Ldk} object containing 11 (x; y) landmarks  from 176 oak leaves wings, from 
+#' @format A \link{Ldk} object containing 11 (x; y) landmarks  from 176 oak leaves wings, from
 #' @source Viscosi, V., & Cardini, A. (2011). Leaf morphology, taxonomy and
 #' geometric morphometrics: a simplified protocol for beginners.
 #' PloS One, 6(10), e25630. doi:10.1371/journal.pone.0025630
 NULL
 
 #' Data: Outline coordinates of 360 molars
-#' 
-#' Courtesy of Julien Corny and Florent Detroit. 
+#'
+#' Courtesy of Julien Corny and Florent Detroit.
 #' @docType data
 #' @name molars
 #' @rdname data_molars
 #' @keywords Datasets
-#' @format A \link{Out} object containing 79 equilinearly spaced (x; y) 
-#' coordinates for 360 crown outlines, of modern human molars, 
-#' along with their type (\code{$type}) - 90 first upper molars (UM1), 90 second upper molars (UM2), 
-#' 90 first lower molars (LM1), 90 second lower molars (LM2) - 
-#' and the individual (\code{ind}) they come from (the data of the 360 molars are taken 
+#' @format A \link{Out} object containing 79 equilinearly spaced (x; y)
+#' coordinates for 360 crown outlines, of modern human molars,
+#' along with their type (\code{$type}) - 90 first upper molars (UM1), 90 second upper molars (UM2),
+#' 90 first lower molars (LM1), 90 second lower molars (LM2) -
+#' and the individual (\code{ind}) they come from (the data of the 360 molars are taken
 #' from 180 individuals).
-#' @source Corny, J., & Detroit, F. (2014). Technical Note: Anatomic 
-#' identification of isolated modern human molars: testing Procrustes aligned 
-#' outlines as a standardization procedure for elliptic fourier analysis. 
+#' @source Corny, J., & Detroit, F. (2014). Technical Note: Anatomic
+#' identification of isolated modern human molars: testing Procrustes aligned
+#' outlines as a standardization procedure for elliptic fourier analysis.
 #' \emph{American Journal of Physical Anthropology}, 153(2), 314-22. doi:10.1002/ajpa.22428
 #' \url{http://onlinelibrary.wiley.com/doi/10.1002/ajpa.22428/abstract}
-NULL 
+NULL
+
+#' Data: Measurement of iris flowers
+#'
+#' @docType data
+#' @name flower
+#' @rdname data_flower
+#' @keywords Datasets
+#' @format A \code{TraCoe} object with 150 measurements of 4 variables
+#' (petal + sepal) x (length x width) on 3 species of iris. This dataset is
+#' the classical \link{iris} formatted for Momocs.
+#' @source see link{iris}
+NULL
+
---
+++ R/cl-def-TraCoe.R
@@ -0,0 +1,29 @@
+#' Traditional morphometrics class
+#'
+#' Defines the builder for traditional measurement class in Momocs.
+#' Is is intended to ease calculations, data handling and multivariate statistics
+#' just ad the other Momocs' classes
+#' @param coe a matrix of measurements
+#' @param fac a data.frame for covariates
+#' @examples
+#' data(iris)
+#' # let's (more or less) rebuidl the flower dataset
+#' TraCoe(iris[, 1:4], data.frame(sp=iris$Species))
+#' @export
+TraCoe <- function(coe = matrix(), fac = data.frame()) {
+  TraCoe <- list(coe = coe, fac = fac)
+  TraCoe <- structure(list(coe = coe, fac = fac), class=c("TraCoe", "Coe"))
+  return(TraCoe)
+}
+
+#' @export
+print.TraCoe <- function(x, ...) {
+  TraCoe <- x
+  ### Header
+  cat("A TraCoe object ", rep("-", 20), "\n", sep = "")
+  shp.nb <- nrow(TraCoe$coe)
+  var.nb <- ncol(TraCoe$coe)
+  cat(" - $coe:", shp.nb, "shapes described with", var.nb, "variables\n")
+  # we print the fac
+  .print.fac(TraCoe$fac)
+}
