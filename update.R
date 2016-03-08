625312629567a1e2db2324f33b3bff9a99e8b63b
vbonhomme <bonhomme.vincent@gmail.com>
2016-03-08 18:16:03 +0100

    version update

--- update.R
+++ update.R
@@ -12,7 +12,7 @@ currDCF[1,"Version"] <- newVersion
 currDCF[1, "Date"] <- strftime(as.POSIXlt(Sys.Date()), "%Y-%m-%d")
 write.dcf(currDCF, "DESCRIPTION")
 system("git add DESCRIPTION")
-cat("Incremented package version\n")
+cat("Incremented package version, now", newVersion, "\n")
 
 #### number of lines
 nl <- numeric()
