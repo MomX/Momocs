d33b9397bb97048053b887d9b48c558198ae3a22
vbonhomme <bonhomme.vincent@gmail.com>
2016-11-22 21:12:28 +0100

    now named prepare_fac

--- R/mult-clust.R
+++ R/mult-clust.R
@@ -115,7 +115,7 @@ CLUST.PCA <- function(x,
 
   # prepares tip.label
   if (!missing(tip_labels))
-    phylo$tip.label <- as.character(.prep.fac(x, tip_labels))
+    phylo$tip.label <- as.character(prepare_fac(x, tip_labels))
 
   # plot the phylo
   plot(phylo, type=type, tip.color=tip.colors, ...)
