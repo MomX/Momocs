09bce397f9f8926ffed74e5d331c8ed4775ac27e
vbonhomme <bonhomme.vincent@gmail.com>
2018-03-21 17:40:29 +0100

    forgot to remove some table*

--- R/cl-handling.R
+++ R/cl-handling.R
@@ -946,17 +946,17 @@ at_least <- function(x, fac, N){
 #' # we select the var Aglan since it is the only one complete
 #' ol <- filter(olea, var == "Aglan")
 #' # everything seems fine
-#' table(ol, "view", "ind")
+#' table(ol$view, ol$ind)
 #' # indeed
 #' rm_uncomplete(ol, id="ind", by="view")
 #'
 #' # we mess the ol object by removing a single shape
 #' ol.pb <- slice(ol, -1)
-#' table(ol.pb, "view", "ind")
+#' table(ol.pb$view, ol.pb$ind)
 #' # the counterpart has been removed with a notice
 #' ol.ok <- rm_uncomplete(ol.pb, "ind", "view")
 #' # now you can combine them
-#' table(ol.ok, "view", "ind")
+#' table(ol.ok$view, ol.ok$ind)
 #' @export
 rm_uncomplete <- function(x, id, by){
   tab <- table(x$fac[, c(id, by)])
